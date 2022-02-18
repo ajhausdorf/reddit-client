5b4950194f4d3de42a74c4865c390deb358ade6c
ajhausdorf <ajhausdorf@wustl.edu>
2022-02-17 22:02:47 -0600

    add scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
         Hello
+        <div className="appwrap">
+          Hello
+          <div className="embed">
+            Hello
+          </div>
+        </div>
     </div>
   );
 }

2108f6ec0eec93d31f2c056985592efbbeb78ebf
ajhausdorf <ajhausdorf@wustl.edu>
2022-02-20 09:20:39 -0600

    add media queries

--- src/features/displayFeedItem/DisplayFeedItem.tsx
+++ src/features/displayFeedItem/DisplayFeedItem.tsx
@@ -5,12 +5,14 @@ const DisplayFeedItem: React.FC = () => {
   return (
     <div className="displayFeedItem">
         <div className="displayFeedItemWrapper">
+            <div className="headingWrapper">
             <div className="feedItemTitle">Dog wins the day</div>
+              <div className="timePosted">7 hours ago</div>
+            </div>
             <img className="feedItemImage" src="https://media.istockphoto.com/photos/cute-dog-put-his-face-on-his-knees-to-the-man-and-smiling-from-the-picture-id1041987488?k=20&m=1041987488&s=612x612&w=0&h=vvR3huxpGzq6fTTQLQ1i-UFKlgwwayRiPeD9vyX0NcQ=" />
             <div className="feedItemInfo">
                 <div className="feedItemInfoWrapper">
                     <div className="postedBy">Posted by: ajhausdorf</div>
-                    <div className="timePosted">7 hours ago</div>
                     <div className="comments"><ModeCommentIcon /> 778</div>
                 </div>
             </div>
